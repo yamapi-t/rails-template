e1b95104936f3f5ddcd4ebee1d754ed270b8a236
tatsumi yamamoto <oreore1256@gmail.com>
2024-09-08 10:46:21 +0900

    chore: Configure letter_opener for development environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
