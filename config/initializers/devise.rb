3499e82a7296fd6620d1dc7efd84d25a65869fd2
tatsumi yamamoto <oreore1256@gmail.com>
2024-09-08 10:24:38 +0900

    chore: Update Google OAuth2 configuration for OmniAuth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,9 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :client_id), Rails.application.credentials.dig(:google, :client_secret_id)
+  config.omniauth :google_oauth2,
+                  Rails.application.credentials.dig(:google, :client_id),
+                  Rails.application.credentials.dig(:google, :client_secret)
   OmniAuth.config.allowed_request_methods = %i[get post]
 
   # ==> Warden configuration
